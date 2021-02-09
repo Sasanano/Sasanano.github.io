6b1e777d611304660411cd02d1bc2568efa8e153
Aleksandr Momziakov <73888668+Sasanano@users.noreply.github.com>
2021-02-09 18:11:51 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -17,15 +17,3 @@ counters.forEach((item, i) => {
     lines[i].style.width = item.innerHTML
 });
 
-$('form').submit(function(e) {
-    e.preventDefault();
-    $.ajax({
-        type: "POST",
-        url: "mailer/smart.php",
-        data: $(this).serialize()
-    }).done(function() {
-        $(this).find("input").val("");
-        $('form').trigger('reset');
-    });
-    return false;
-});
